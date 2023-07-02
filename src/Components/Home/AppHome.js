96721f7f6d7e355d04814debf548e96b1654c331
backprop <backprop@users.noreply.github.com>
2023-07-02 14:01:32 -0500

    updating home page

--- src/Components/Home/AppHome.js
+++ src/Components/Home/AppHome.js
@@ -1,10 +1,14 @@
 import React from "react"
 import "./AppHome.css"
+import me from "../../assets/images/photos/me.jpg";
 
 export default function AppHome(){
     return(
-        <div>
+        <section id="home--main">
+
             This is my app where I add a bunch of different demos so that I have a quick reference to many different smaller pieces of React
-        </div>
+        
+            
+        </section>
     )
 }
